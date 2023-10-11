e8e9a8632699a7a96aa19071dbe0f3c4a4b018c7
ChesterZhao <saymealoud@gmail.com>
2023-10-11 21:57:42 +0800

    计算这个站卖出去后,影响哪些站的余票库存

--- business/src/main/java/com/jiawa/train/business/service/ConfirmOrderService.java
+++ business/src/main/java/com/jiawa/train/business/service/ConfirmOrderService.java
@@ -50,6 +50,9 @@ public class ConfirmOrderService {
     @Resource
     DailyTrainSeatService dailyTrainSeatService;
 
+    @Resource
+    private AfterConfirmOrderService afterConfirmOrderService;
+
     public void save(ConfirmOrderDoReq req) {
         DateTime now = DateTime.now();
         ConfirmOrder confirmOrder = BeanUtil.copyProperties(req, ConfirmOrder.class);
@@ -183,13 +186,16 @@ public class ConfirmOrderService {
 
         LOG.info("最终选座: {}", finalSeatList);
 
-        // 一个车箱一个车箱的获取座位数据
-
-            // 挑选符合条件的座位，如果这个车箱不满足，则进入下个车箱（多个选座应该在同一个车厢）
-
         // 选中座位后事务处理：
+        try {
+            afterConfirmOrderService.afterDoConfirm(dailyTrainTicket, finalSeatList, tickets, confirmOrder);
+        } catch (Exception e) {
+            LOG.error("保存购票信息失败", e);
+            throw new BusinessException(BusinessExceptionEnum.CONFIRM_ORDER_EXCEPTION);
+        }
 
-            // 座位表修改售卖情况sell；
+        // 座位表修改售卖情况sell;
+
             // 余票详情表修改余票；
             // 为会员增加购票记录
             // 更新确认订单为成功
