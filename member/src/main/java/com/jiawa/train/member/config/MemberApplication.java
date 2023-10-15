b744bb167ec08fd80135d0300b02a05b82442600
ChesterZhao <saymealoud@gmail.com>
2023-10-15 15:36:56 +0800

    集成nacos 配置中心

--- member/src/main/java/com/jiawa/train/member/config/MemberApplication.java
+++ member/src/main/java/com/jiawa/train/member/config/MemberApplication.java
@@ -5,12 +5,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan("com.jiawa")
 @MapperScan("com.jiawa.train.*.mapper")
+@RefreshScope
 public class MemberApplication {
 
     private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
