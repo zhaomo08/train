590ded123f21a2325daef76c0f66961b83773f29
ChesterZhao <saymealoud@gmail.com>
2023-10-15 18:01:11 +0800

    配置 feign + nacos  实现应用名远程调用

--- batch/src/main/java/com/jiawa/train/batch/controller/TestController.java
+++ batch/src/main/java/com/jiawa/train/batch/controller/TestController.java
@@ -19,6 +19,6 @@ public class TestController {
     public String hello() {
         String businessHello = businessFeign.hello();
         LOG.info(businessHello);
-        return "Hello World! Batch!";
+        return "Hello World! Batch!" + businessHello;
     }
 }
