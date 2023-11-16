b542fa5eeaa73b59bd6a219d6af4d1f2747d537e
ChesterZhao <saymealoud@gmail.com>
2023-11-16 22:09:13 +0800

    令牌限流CRUD生成提交

---
+++ business/src/main/java/com/jiawa/train/business/controller/admin/skTokenAdminController.java
@@ -0,0 +1,38 @@
+package com.jiawa.train.business.controller.admin;
+
+import com.jiawa.train.business.req.skTokenQueryReq;
+import com.jiawa.train.business.req.skTokenSaveReq;
+import com.jiawa.train.business.resp.skTokenQueryResp;
+import com.jiawa.train.business.service.skTokenService;
+import com.jiawa.train.common.resp.CommonResp;
+import com.jiawa.train.common.resp.PageResp;
+import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/admin/sk-token")
+public class skTokenAdminController {
+
+    @Resource
+    private skTokenService skTokenService;
+
+    @PostMapping("/save")
+    public CommonResp<Object> save(@Valid @RequestBody skTokenSaveReq req) {
+        skTokenService.save(req);
+        return new CommonResp<>();
+    }
+
+    @GetMapping("/query-list")
+    public CommonResp<PageResp<skTokenQueryResp>> queryList(@Valid skTokenQueryReq req) {
+        PageResp<skTokenQueryResp> list = skTokenService.queryList(req);
+        return new CommonResp<>(list);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public CommonResp<Object> delete(@PathVariable Long id) {
+        skTokenService.delete(id);
+        return new CommonResp<>();
+    }
+
+}
---
+++ business/src/main/java/com/jiawa/train/business/req/skTokenQueryReq.java
@@ -0,0 +1,12 @@
+package com.jiawa.train.business.req;
+
+import com.jiawa.train.common.req.PageReq;
+
+public class skTokenQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "skTokenQueryReq{" +
+                "} " + super.toString();
+    }
+}
